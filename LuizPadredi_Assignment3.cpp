d674dc7eb4cd4cd7ed3c2ddeba560fd53be40b84
Padredilg <padredilg@knights.ucf.edu>
2023-02-28 21:28:38 -0500

    initial cpp file

---
+++ LuizPadredi_Assignment3.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Luiz Padredi
+Date: 02/28/2023
+Assignment 3: Mock Classroom Grades
+Repository: https://github.com/Padredilg/mock-classroom-grades
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+    return 0;
+}
